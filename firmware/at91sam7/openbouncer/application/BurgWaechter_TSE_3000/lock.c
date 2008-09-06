8a950a4686fe8bde998c415d3034992c39c26f7c
Henryk Ploetz <henryk@ploetzli.ch>
2008-09-06 17:44:07 +0000

    Working bit-banging manchester output code

--- firmware/at91sam7/openbouncer/application/BurgWaechter_TSE_3000/lock.c
+++ firmware/at91sam7/openbouncer/application/BurgWaechter_TSE_3000/lock.c
@@ -32,11 +32,11 @@
 #include "../cmd/usbio.h"
 
 /**********************************************************************/
-#define cylinderTelegramLen			16
+#define cylinderTelegramLen			8
 int cylinderTelegram[cylinderTelegramLen] = { 0x02, 0x12, 0x34, 0x56,
   0x00, 0x00, 0x5c, 0x00,
-  0x02, 0x04, 0x00, 0x00,
-  0x00, 0x00, 0x80, 0x00
+  //0x02, 0x04, 0x00, 0x00,
+  //0x00, 0x00, 0x80, 0x00
 };
 
 //#define TICKS_PER_50_MSECOND                  0.0005*MCK/2
@@ -46,42 +46,28 @@ xSemaphoreHandle waitSemaphore;
 
 /**********************************************************************/
 
-/* Write cylinder status */
-void
-vWait500ms (void)
+static void
+waitForNext500msSlot (void)
 {
-  xSemaphoreTake(waitSemaphore, 0); // Clear semaphore 
+  (void) AT91C_BASE_TC0->TC_SR;
+  while (!(AT91C_BASE_TC0->TC_SR & AT91C_TC_CPCS));
+}
+
+static void
+startTimer (void)
+{
   AT91C_BASE_TC0->TC_CCR = AT91C_TC_CLKEN | AT91C_TC_SWTRG;	// start counter
-  AT91F_AIC_ClearIt(AT91C_ID_TC0);
-  AT91F_AIC_EnableIt(AT91C_ID_TC0);
-  xSemaphoreTake(waitSemaphore, 10*portTICK_RATE_MS); // Will wait for the semaphore to be free again
-  vLedSetGreen(0);
-  AT91F_AIC_DisableIt (AT91C_ID_TC0);
+}
+
+static void
+stopTimer (void)
+{
   AT91C_BASE_TC0->TC_CCR = AT91C_TC_CLKDIS;
-}
-
-void
-vTimerWaitISR (void)
-{
-  portBASE_TYPE taskWoken = pdFALSE;
-  if (AT91C_BASE_TC0->TC_SR & AT91C_TC_CPCS)
-    xSemaphoreGiveFromISR (waitSemaphore, &taskWoken);
-  vLedSetGreen(1);
-  AT91F_AIC_AcknowledgeIt ();
-  if (taskWoken == pdTRUE)
-    portYIELD_FROM_ISR ();
-}
-
-void __attribute__ ((naked)) vTimerWaitISR_Handler (void)
-{
-  portSAVE_CONTEXT ();
-  vTimerWaitISR ();
-  portRESTORE_CONTEXT ();
 }
 
 /**********************************************************************/
 /* Write cylinder status */
-void
+static void
 vWriteCylinderLine (int status)
 {
 //int i;
@@ -95,7 +81,7 @@ vWriteCylinderLine (int status)
       AT91F_PIO_CfgOutput (AT91C_BASE_PIOA, CYLINDER_IO);	/* Line low */
     }
 //for( i=0; i<1400; i++);
-  vWait500ms ();
+  waitForNext500msSlot ();
   return;
 }
 
@@ -107,6 +93,7 @@ sendCylinderTelegram (int *telegram, int length)
 {
   int i, j = 0;
 
+  startTimer ();
   /* Wakeup Pulse */
   /* generate logical HIGH */
   vWriteCylinderLine (1);
@@ -115,8 +102,9 @@ sendCylinderTelegram (int *telegram, int length)
   vWriteCylinderLine (0);
   /* generate logical HIGH */
   vWriteCylinderLine (1);
-  vTaskDelay (60);		// in ms
+  vTaskDelay (57);		// in ms
 
+  startTimer ();
   /* Telegram */
   for (j = 0; j <= length; j++)
     {
@@ -140,6 +128,7 @@ sendCylinderTelegram (int *telegram, int length)
 
   /* generate logical HIGH */
   vWriteCylinderLine (1);
+  stopTimer ();
 }
 
 /**********************************************************************/
@@ -158,28 +147,14 @@ vLockInit (void)
   AT91F_PIO_CfgInput (AT91C_BASE_PIOA, CYLINDER_IO);
   AT91F_PIO_ClearOutput (AT91C_BASE_PIOA, CYLINDER_IO);
 
-  vSemaphoreCreateBinary (waitSemaphore);
-  if (waitSemaphore == NULL)
-    {
-      // Stirb.
-    }
-  if (xSemaphoreTake (waitSemaphore, 1) != pdTRUE)
-    {
-      // Stirb harder.
-    }
-
-  AT91F_AIC_ConfigureIt (AT91C_ID_TC0, 4, AT91C_AIC_SRCTYPE_INT_POSITIVE_EDGE,
-			 vTimerWaitISR_Handler);
   AT91F_PMC_EnablePeriphClock (AT91C_BASE_PMC,
 			       ((unsigned int) 1 << AT91C_ID_TC0));
 
   AT91C_BASE_TCB->TCB_BMR = 0;
   AT91C_BASE_TC0->TC_CMR = AT91C_TC_CLKS_TIMER_DIV1_CLOCK	// MCK/2
     | AT91C_TC_BURST_NONE	// The clock is not gated by an external signal
-    | AT91C_TC_CPCSTOP		// Counter Clock Stopped with RC Compare
     | AT91C_TC_WAVESEL_UP_AUTO;	// UP mode with automatic trigger on RC Compare
   AT91C_BASE_TC0->TC_RC = TICKS_PER_500_MSECOND;	// TC0 Timer interval 500ms
   AT91C_BASE_TC0->TC_CCR = AT91C_TC_CLKDIS;
-  AT91C_BASE_TC0->TC_IER = AT91C_TC_CPCS;
   return 0;
 }
