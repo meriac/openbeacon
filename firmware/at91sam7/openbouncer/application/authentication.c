83515fe862b6ba207be24cc579dd7155926fa624
Henryk Ploetz <henryk@ploetzli.ch>
2008-09-16 18:35:11 +0000

    Fix types

--- firmware/at91sam7/openbouncer/application/authentication.c
+++ firmware/at91sam7/openbouncer/application/authentication.c
@@ -1,13 +1,13 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <string.h>
-#include <beacontypes.h>
+#include <stdint.h>
 
 #include "authentication.h"
 #include "openbouncer.h"
 #include "board.h"
 
-u_int8_t test;
+uint8_t test;
 
 struct
 {
@@ -21,16 +21,16 @@ struct
     AUTHENTICATION_ACCEPTED,
     AUTHENTICATION_FAILED,
   } state;
-  u_int8_t hello_salt[4];
-  u_int8_t tag_retries;
-  u_int8_t setup[8];
-  u_int8_t challenge[8];
+  uint8_t hello_salt[4];
+  uint8_t tag_retries;
+  uint8_t setup[8];
+  uint8_t challenge[8];
   unsigned int authentication_time;
 } authentication_state;
 
 struct tag_key
 {
-  u_int8_t KEY[16];
+  uint8_t KEY[16];
   unsigned int refcount;
 };
 
