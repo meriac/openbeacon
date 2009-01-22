d0149f13485a808b4458478c89d1bbdd00afc1f3
Henryk Ploetz <henryk@ploetzli.ch>
2009-01-22 23:04:54 +0000

    PICC target demo working

--- firmware/at91sam7/openbeacon-openpicc2/application/nfc/pn532_demo.c
+++ firmware/at91sam7/openbeacon-openpicc2/application/nfc/pn532_demo.c
@@ -33,12 +33,12 @@ void pn532_demo_task(void *parameter)
 	}
 
 	{
-#if 0
+#if 1
 		const char cmd[] = {0xd4, 0x8c,
 				0x04,
-				0x80, 0x00,
+				0x08, 0x00,
 				0x12, 0x34, 0x56,
-				0x60,
+				0x20,
 				0x01, 0xfe, 0xa2, 0xa3,
 				0xa4, 0xa5, 0xa6, 0xa7,
 				0xc0, 0xc1, 0xc2, 0xc3,
@@ -48,8 +48,8 @@ void pn532_demo_task(void *parameter)
 				0x77, 0x66, 0x55, 0x44,
 				0x33, 0x22, 0x11,
 				0x00,
-				0x00}; // TgInitAsTarget
-#elif 1
+				0x02, 0x12, 0x34}; // TgInitAsTarget
+#elif 0
 		const char cmd[] = {0xd4, 0x8c,
 				0x00,
 				0x80, 0x00,
@@ -81,7 +81,16 @@ void pn532_demo_task(void *parameter)
 	if(pn532_send_frame(msg)==0) printf("Command sent and ack'ed\n");
 	pn532_put_message_buffer(&msg);
 
-	if(0){
+	if(pn532_recv_frame_queue(&msg, queue) == 0) {
+		DumpUIntToUSB((unsigned int)msg);
+		printf(" Message received here, too %i %i %i\n", msg->type, msg->payload_len, msg->state);
+		{int i; for(i=0; i<msg->payload_len; i++) printf("%02X ", msg->message.data[i]); printf("\n");}
+		pn532_put_message_buffer(&msg);
+	}
+
+	vTaskDelay(1000/portTICK_RATE_MS);
+
+	{
 		const char cmd[] = {0xd4, 0x86};
 		if( pn532_get_message_buffer(&msg) == 0) {
 			pn532_prepare_frame(msg, cmd, sizeof(cmd));
@@ -92,15 +101,6 @@ void pn532_demo_task(void *parameter)
 			printf("Couldn't 0x86\n");
 		}
 	}
-
-	if(pn532_recv_frame_queue(&msg, queue) == 0) {
-		DumpUIntToUSB((unsigned int)msg);
-		printf(" Message received here, too %i %i %i\n", msg->type, msg->payload_len, msg->state);
-		{int i; for(i=0; i<msg->payload_len; i++) printf("%02X ", msg->message.data[i]); printf("\n");}
-		pn532_put_message_buffer(&msg);
-	}
-
-	vTaskDelay(1000/portTICK_RATE_MS);
 
 	while(1) {
 		if(pn532_recv_frame_queue(&msg, queue) == 0) {
