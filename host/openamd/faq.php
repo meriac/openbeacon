59769cedd255947ae2a376662fd1f1faaa653297
Aestetix <aestetix@gmail.com>
2009-02-08 21:05:14 +0000

    adding code from 25c3

--- host/openamd/faq.php
+++ host/openamd/faq.php
@@ -27,10 +27,12 @@
 
 include("header.php"); ?>
 
-<h1>faq</h1>
+<div class="body-header">The FAQ<br></div>
+<div class="body-text" style="width: 550px">
 <a href="#amd">What is the OpenAMD project?</a><br>
 <a href="#rfid">What is RFID? How does it track me?</a><br>
 <a href="#tracking">What, specifically, are you tracking?</a><br>
+<a href="#oldbeacon">Can I use the Sputnik Tag from an earlier congress?</a><br>
 <a href="#interesttags">What are interest tags? How are they being used in OpenAMD?</a><br>
 <a href="info">Why do you want all this other into? What are you doing with it?</a><br>
 <a href="#important">Why is all this important?</a><br>
@@ -38,40 +40,41 @@ include("header.php"); ?>
 <a href="#sponsors">Sponsors and Friends</a>
 <br><br>
 <ul>
-<li><a name="amd">What is the OpenAMD project?</a></li>
+<li><a name="amd"><b>What is the OpenAMD project?</b></a></li>
 The Open Attendee Meta-Data Project (OpenAMD) takes "RFID", an emerging hardware technology, and shows how it can be used in a socially friendly way.  By combining RFID tracking with a social networking system that uses "interest tagging", we not only give people a better conference experience, but also allow them to connect with other users.
-<Br>
-<li><a name="rfid">What is RFID? How does it track me?</a></li>
-RFID (radio frequency identification) is a technology that uses a chip to broadcast a detection point.  In this case we are using active RFID chips, which emit a radio pulse that is picked up by readers placed in the conference areas.  These chips will be found on your HOPE admission badge if you have preregistered.  Each user's pulse is recorded in a database and we can use it to follow people around the conference.  The tracking is only present in specified conference areas (users not on the Mezzanine level or the 18th floor of the hotel will not be tracked).
 <br>
-<li><a name="tracking">What, specifically, are you tracking?  If i walk into the conference, do you automatically have my name/address/credit card number/last blood test results?</a></li>
+<li><a name="rfid"><b>What is RFID? How does it track me?</b></a></li>
+RFID (radio frequency identification) is a technology that uses a chip to broadcast a detection point.  In this case we are using active RFID chips, which emit a radio pulse that is picked up by readers placed in the conference areas.  These chips will be found on your OpenAMD badge if you have preregistered.  Each user's pulse is recorded in a database and we can use it to follow people around the conference.  The tracking is only present in specified conference areas (users not on the Mezzanine level or the 18th floor of the hotel will not be tracked).
+<br>
+<li><a name="tracking"><b>What, specifically, are you tracking?  If i walk into the conference, do you automatically have my name/address/credit card number/last blood test results?</b></a></li>
 When a user is wearing the badge, the only things being tracked are badge number and location.  We do not have any other user information. If you choose to enter info on the site, all that info is being stored external to the badge, on our database.
 <br>
-<li><a name="interesttags">What are interest tags? How are they being used in OpenAMD?</a></li>
+<li><a name="oldbeacon"><b>Can I use the Sputnik Tag from an earlier congress?</b></a></li>
+Yes, you can if you own a quare-shaped model (from 23C3, 24C3). But you have to come to the Sputnik desk to get your Firmware updated. If you have a round-shaped model (from the CCCamp 2007) you need a new tag.
+<br>
+<li><a name="interesttags"><b>What are interest tags? How are they being used in OpenAMD?</b></a></li>
 Interest tags are keywords used to tag users in much the same manner a site such as Flickr uses tags to identify and sort images.  Users can choose to login to the OpenAMD website and select up to five interests, which they can be sorted by.  These interests can be used to determine other people you may want to meet.
 <br>
-<li><a name="info">Why do you want all this other info? What are you doing with it?</a></li>
+<li><a name="info"><b>Why do you want all this other info? What are you doing with it?</b></a></li>
 All personal information we collect is voluntary; however, the information we can collect will help enhance your convention experience.  Hometown location can be used for meeting others from your area.  Your cell number can provide you with text message reminders of talks you wish to catch, or of events you plan to attend.  Your interest info can not only allow you to streamline your choices of talks to what you are most interested in, but will help group you with others who share your interests during the games.
 <br><br>
-We absolutely are not going to release this information to any third party, or to anyone outside the project itself. All info you submit is confidential within the bounds of the OpenAMD project and the HOPE conference.  Anonymous location data will be released after the conference, but this data will not include any personal data.
+We absolutely are not going to release this information to any third party, or to anyone outside the project itself. All info you submit is confidential within the bounds of the OpenAMD project and the 25c3 conference.  Anonymous location data will be released after the conference, but this data will not include any personal data.
 <br>
-<li><a name="important">Why is all this important?</a></li>
+<li><a name="important"><b>Why is all this important?</b></a></li>
 This project is designed to explore RFID technology, a tech that is usually considering intimidating and flavored with Big Brother-style abilities, in a transparent and accessible manner.  We want to both stretch the limits of how RFID can be applied, and make people aware of how it works and what it's like to work with it.  We are presenting this project to the public both in order to mitigate the fear response many people will have to using RFID widely, and to encourage people to embrace it and take the technology into their own hands.  If we do not, then who will?
 <br>
-<li><a name="who">Who are we?</a></li>
-Lexicon: Project Manager<br>
-Ragefury: Project Manager, Infrastructure<br>
-Dementia: Project Manager, Infrastructure<br>
-Daravinne: Project Manager, Visualization<br>
-Aestetix: Database<br>
-Wankel: Database<br>
-CPFR: Database<br>
-<br>
-<li><a name="sponsors">Sponsors and Friends</a></li>
-Paid sponsors.<br>
+<li><a name="who"><b>Who are we?</b></a></li>
+Milosch and Brita Meriac<br>
+Ciro Circutto<br>
+Wouter Van den Broeck<br>
+Eric<br>
+Ben Maus<br>
+aestetix<br>
+PHPat<br>
+<li><a name="sponsors"><b>Sponsors and Friends</b></a></li>
 <a href="http://www.2600.com">2600.com</a> and Emmanuel Goldstein.
-
+<br><br><br><br>
 </ul>
+</div>
 
-
-<?php include("footer.php"); ?>
+	<?php include("footer.php"); ?>
