cae951fc82b61053b91c4bcdacf1629b0f4c639b
Henryk Ploetz <henryk@ploetzli.ch>
2009-06-11 13:33:27 +0000

    Sanitize indentation

--- firmware/at91sam7/openbeacon-openpicc2/application/main.c
+++ firmware/at91sam7/openbeacon-openpicc2/application/main.c
@@ -91,28 +91,26 @@ vApplicationIdleHook (void)
 void
 watchdog_restart_task (void *parameter)
 {
-  (void) parameter;
-  while (1)
-    {
-      /* Restart watchdog, has been enabled in Cstartup_SAM7.c */
-      AT91F_WDTRestart (AT91C_BASE_WDTC);
-      vTaskDelay (500 / portTICK_RATE_MS);
-    }
+	(void) parameter;
+	while (1) {
+		/* Restart watchdog, has been enabled in Cstartup_SAM7.c */
+		AT91F_WDTRestart (AT91C_BASE_WDTC);
+		vTaskDelay (500 / portTICK_RATE_MS);
+	}
 }
 
 void
 demo_task (void *parameter)
 {
-  (void) parameter;
-
-  led_set_red (1);
-  vTaskDelay (500 / portTICK_RATE_MS);
-  led_set_red (0);
-
-  while (1)
-    {
-      vTaskDelay (1000 / portTICK_RATE_MS);
-    }
+	(void) parameter;
+	
+	led_set_red (1);
+	vTaskDelay (500 / portTICK_RATE_MS);
+	led_set_red (0);
+	
+	while (1) {
+		vTaskDelay (1000 / portTICK_RATE_MS);
+	}
 }
 
 /**********************************************************************/
@@ -120,7 +118,7 @@ demo_task (void *parameter)
 static inline unsigned char
 HexChar (unsigned char nibble)
 {
-  return nibble + ((nibble < 0x0A) ? '0' : ('A' - 0xA));
+	return nibble + ((nibble < 0x0A) ? '0' : ('A' - 0xA));
 }
 
 /**********************************************************************/
@@ -293,139 +291,124 @@ void eink_traffic_test_task (void *parameter)
 void
 hex_dump (const unsigned char *buf, unsigned int addr, unsigned int len)
 {
-  unsigned int start, i, j;
-  char c;
-
-  start = addr & ~0xf;
-
-  for (j = 0; j < len; j += 16)
-    {
-      printf ("%08x:", start + j);
-
-      for (i = 0; i < 16; i++)
-	{
-	  if (start + i + j >= addr && start + i + j < addr + len)
-	    printf (" %02x", buf[start + i + j]);
-	  else
-	    printf ("   ");
+	unsigned int start, i, j;
+	char c;
+	
+	start = addr & ~0xf;
+	
+	for (j = 0; j < len; j += 16) {
+		printf ("%08x:", start + j);
+		
+		for (i = 0; i < 16; i++) {
+			if (start + i + j >= addr && start + i + j < addr + len) {
+				printf (" %02x", buf[start + i + j]);
+			} else {
+				printf ("   ");
+			}
+		}
+		printf ("  |");
+		for (i = 0; i < 16; i++) {
+			if (start + i + j >= addr && start + i + j < addr + len) {
+				c = buf[start + i + j];
+				if (c >= ' ' && c < 127) {
+					printf ("%c", c);
+				} else {
+					printf (".");
+				}
+			} else {
+				printf (" ");
+			}
+		}
+		printf ("|\n");
 	}
-      printf ("  |");
-      for (i = 0; i < 16; i++)
-	{
-	  if (start + i + j >= addr && start + i + j < addr + len)
-	    {
-	      c = buf[start + i + j];
-	      if (c >= ' ' && c < 127)
-		printf ("%c", c);
-	      else
-		printf (".");
-	    }
-	  else
-	    printf (" ");
-	}
-      printf ("|\n");
-    }
 }
 
-void
-sdcard_test_task (void *parameter)
+void sdcard_test_task (void *parameter)
 {
-  u_int32_t pstart, psize, i,t;
-  u_int8_t pactive, ptype;
-  VOLINFO vi;
-  DIRINFO di;
-  DIRENT de;
-  FILEINFO fi;
-//  u_int32_t cache;
-  int res;
-
-  (void) parameter;
-
-
-  vTaskDelay (5000 / portTICK_RATE_MS);
-  printf ("Here we go\n");
-
-  if ((res = DFS_OpenCard ()) != 0)
-    {
-      printf ("Can't open SDCARD [%i]\n", res);
-      while (1)
-	vTaskDelay (100 / portTICK_RATE_MS);
-    }
-  else
-    {
-      // Obtain pointer to first partition on first (only) unit
-      pstart = DFS_GetPtnStart (0, sector, 0, &pactive, &ptype, &psize);
-      if (pstart == 0xffffffff)
-	{
-	  printf ("Cannot find first partition\n");
-	  hex_dump(sector,0,sizeof(sector));
-	  while (1)
-	    vTaskDelay (100 / portTICK_RATE_MS);
+	u_int32_t pstart, psize, i,t;
+	u_int8_t pactive, ptype;
+	VOLINFO vi;
+	DIRINFO di;
+	DIRENT de;
+	FILEINFO fi;
+	//  u_int32_t cache;
+	int res;
+	
+	(void) parameter;
+	
+	
+	vTaskDelay (5000 / portTICK_RATE_MS);
+	printf ("Here we go\n");
+	
+	if ((res = DFS_OpenCard ()) != 0) {
+		printf ("Can't open SDCARD [%i]\n", res);
+		while (1)
+			vTaskDelay (100 / portTICK_RATE_MS);
+	} else {
+		// Obtain pointer to first partition on first (only) unit
+		pstart = DFS_GetPtnStart (0, sector, 0, &pactive, &ptype, &psize);
+		if (pstart == 0xffffffff) {
+			printf ("Cannot find first partition\n");
+			hex_dump(sector,0,sizeof(sector));
+			while(1) vTaskDelay (100 / portTICK_RATE_MS);
+		}
+		
+		printf ("Partition 0 start sector 0x%08X active 0x%02X type 0x%02X size 0x%08X\n",
+				(unsigned int) pstart, (unsigned int) pactive, (unsigned int) ptype,
+				(unsigned int) psize);
+		
+		if (DFS_GetVolInfo (0, sector, pstart, &vi)) {
+			printf ("Error getting volume information\n");
+			while(1) vTaskDelay (100 / portTICK_RATE_MS);
+		}
+		printf ("Volume label '%-11.11s'\n", vi.label);
+		printf ("%d sector/s per cluster, %d reserved sector/s, volume total %d sectors.\n",
+				(int) vi.secperclus, (int) vi.reservedsecs, (int) vi.numsecs);
+		printf ("%d sectors per FAT, first FAT at sector #%d, root dir at #%d.\n",
+				(int) vi.secperfat, (int) vi.fat1, (int) vi.rootdir);
+		printf ("(For FAT32, the root dir is a CLUSTER number, FAT12/16 it is a SECTOR number)\n");
+		printf ("%d root dir entries, data area commences at sector #%d.\n",
+				(int) vi.rootentries, (int) vi.dataarea);
+		printf ("%d clusters (%d bytes) in data area, filesystem IDd as ",
+				(int) vi.numclusters,
+				(int) vi.numclusters * vi.secperclus * SECTOR_SIZE);
+		
+		if (vi.filesystem == FAT12) {
+			printf ("FAT12.\n");
+		} else if (vi.filesystem == FAT16) {
+			printf ("FAT16.\n");
+		} else if (vi.filesystem == FAT32) {
+			printf ("FAT32.\n");
+		} else {
+			printf ("[unknown]\n");
+		}
+		
+		di.scratch = sector1;
+		if (DFS_OpenDir (&vi, (uint8_t *) "", &di)) {
+			printf ("Error opening root directory\n");
+			while(1) vTaskDelay (100 / portTICK_RATE_MS);
+		}
+		
+		while (!DFS_GetNext (&vi, &di, &de)) {
+			if (de.name[0]) {
+				printf ("file: '%-11.11s'\n", de.name);
+				printf ("read test:\n");
+				if (DFS_OpenFile (&vi, de.name, DFS_READ, sector, &fi)) {
+					printf ("error opening file\n");
+				} else {
+					t = fi.filelen;
+					if(t>SECTOR_SIZE)
+						t=SECTOR_SIZE;
+					DFS_ReadFile (&fi, sector, buffer, &i, t);
+					hex_dump(buffer,0,i);
+				}
+			}
+		}
+		
+		
+		printf("DONE\n");
+		while(1) vTaskDelay (100 / portTICK_RATE_MS);
 	}
-
-      printf ("Partition 0 start sector 0x%08X active 0x%02X type 0x%02X size 0x%08X\n",
-	 (unsigned int) pstart, (unsigned int) pactive, (unsigned int) ptype,
-	 (unsigned int) psize);
-
-      if (DFS_GetVolInfo (0, sector, pstart, &vi))
-	{
-	  printf ("Error getting volume information\n");
-	  while (1)
-	    vTaskDelay (100 / portTICK_RATE_MS);
-	}
-      printf ("Volume label '%-11.11s'\n", vi.label);
-      printf ("%d sector/s per cluster, %d reserved sector/s, volume total %d sectors.\n",
-	 (int) vi.secperclus, (int) vi.reservedsecs, (int) vi.numsecs);
-      printf ("%d sectors per FAT, first FAT at sector #%d, root dir at #%d.\n",
-	 (int) vi.secperfat, (int) vi.fat1, (int) vi.rootdir);
-      printf ("(For FAT32, the root dir is a CLUSTER number, FAT12/16 it is a SECTOR number)\n");
-      printf ("%d root dir entries, data area commences at sector #%d.\n",
-	      (int) vi.rootentries, (int) vi.dataarea);
-      printf ("%d clusters (%d bytes) in data area, filesystem IDd as ",
-	      (int) vi.numclusters,
-	      (int) vi.numclusters * vi.secperclus * SECTOR_SIZE);
-
-      if (vi.filesystem == FAT12)
-	printf ("FAT12.\n");
-      else if (vi.filesystem == FAT16)
-	printf ("FAT16.\n");
-      else if (vi.filesystem == FAT32)
-	printf ("FAT32.\n");
-      else
-	printf ("[unknown]\n");
-
-      di.scratch = sector1;
-      if (DFS_OpenDir (&vi, (uint8_t *) "", &di))
-	{
-	  printf ("Error opening root directory\n");
-	  while (1)
-	    vTaskDelay (100 / portTICK_RATE_MS);
-	}
-
-      while (!DFS_GetNext (&vi, &di, &de))
-	{
-	  if (de.name[0])
-	  {
-	    printf ("file: '%-11.11s'\n", de.name);
-	    printf ("read test:\n");
-	    if (DFS_OpenFile (&vi, de.name, DFS_READ, sector, &fi))
-		printf ("error opening file\n");
-	    else
-	    {
-		t = fi.filelen;
-		if(t>SECTOR_SIZE)
-		    t=SECTOR_SIZE;
-		DFS_ReadFile (&fi, sector, buffer, &i, t);
-		hex_dump(buffer,0,i);
-	    }
-	  }
-	}
-
-
-      printf("DONE\n");
-      while (1)
-	vTaskDelay (100 / portTICK_RATE_MS);
-    }
 }
 
 extern int eink_comm_test (u_int16_t reg1, u_int16_t reg2);
@@ -434,58 +417,50 @@ extern u_int16_t eink_read_register (u_int16_t reg);
 void
 flash_demo_task (void *parameter)
 {
-  (void) parameter;
-  vTaskDelay (3000 / portTICK_RATE_MS);
-  printf ("Here\n");
-
-  int r = eink_flash_acquire ();
-  if (r >= 0)
-    {
-      unsigned int ident = eink_flash_read_identification ();
-      printf ("Device identification: %06X\n", ident);
-
-      printf ("Starting read\n");
-      eink_flash_read (0, (char *) SDRAM_BASE, READ_SIZE);
-      eink_flash_release ();
-      printf ("Dunnit\n");
-
-    }
-  else
-    {
-      switch ((enum eink_error) -r)
-	{
-	case EINK_ERROR_NOT_DETECTED:
-	  printf ("eInk controller not detected\n");
-	  break;
-	case EINK_ERROR_NOT_SUPPORTED:
-	  printf ("eInk controller not supported (unknown revision)\n");
-	  break;
-	case EINK_ERROR_COMMUNICATIONS_FAILURE:
-	  printf
-	    ("eInk controller communications failure, check FPC cable and connector\n");
-	  break;
-	case EINK_ERROR_CHECKSUM_ERROR:
-	  printf ("eInk controller waveform flash checksum failure\n");
-	  break;
-	default:
-	  printf ("eInk controller initialization: unknown error\n");
-	  break;
+	(void) parameter;
+	vTaskDelay (3000 / portTICK_RATE_MS);
+	printf ("Here\n");
+	
+	int r = eink_flash_acquire ();
+	if (r >= 0) {
+		unsigned int ident = eink_flash_read_identification ();
+		printf ("Device identification: %06X\n", ident);
+		
+		printf ("Starting read\n");
+		eink_flash_read (0, (char *) SDRAM_BASE, READ_SIZE);
+		eink_flash_release ();
+		printf ("Dunnit\n");
+		
+	} else {
+		switch ((enum eink_error) -r) {
+		case EINK_ERROR_NOT_DETECTED:
+			printf ("eInk controller not detected\n");
+			break;
+		case EINK_ERROR_NOT_SUPPORTED:
+			printf ("eInk controller not supported (unknown revision)\n");
+			break;
+		case EINK_ERROR_COMMUNICATIONS_FAILURE:
+			printf
+			("eInk controller communications failure, check FPC cable and connector\n");
+			break;
+		case EINK_ERROR_CHECKSUM_ERROR:
+			printf ("eInk controller waveform flash checksum failure\n");
+			break;
+		default:
+			printf ("eInk controller initialization: unknown error\n");
+			break;
+		}
+		if (eink_comm_test (0x148, 0x156)) {
+			printf ("Comm test ok\n");
+		} else {
+			printf ("Comm test not ok\n");
+			printf ("%04X\n", eink_read_register (0x148));
+		}
 	}
-      if (eink_comm_test (0x148, 0x156))
-	{
-	  printf ("Comm test ok\n");
+	
+	while (1) {
+		vTaskDelay (1000 / portTICK_RATE_MS);
 	}
-      else
-	{
-	  printf ("Comm test not ok\n");
-	  printf ("%04X\n", eink_read_register (0x148));
-	}
-    }
-
-  while (1)
-    {
-      vTaskDelay (1000 / portTICK_RATE_MS);
-    }
 }
 
 
@@ -564,70 +539,69 @@ void sdram_clear(void)
 /**********************************************************************/
 void __attribute__((noreturn)) mainloop (void)
 {
-  prvSetupHardware ();
-  AT91F_RSTSetMode(AT91C_BASE_RSTC, AT91F_RSTGetMode(AT91C_BASE_RSTC) | 0x11 );
-
-  led_init ();
-
-  detect_board();
-
-  sdram_init ();
-  if(0) { /* This takes a lot of power and should only be done with a connected battery.
+	prvSetupHardware ();
+	AT91F_RSTSetMode(AT91C_BASE_RSTC, AT91F_RSTGetMode(AT91C_BASE_RSTC) | 0x11 );
+	
+	led_init ();
+	
+	detect_board();
+	
+	sdram_init ();
+	if(0) { /* This takes a lot of power and should only be done with a connected battery.
 			   Note: Since it writes to SDRAM and the tasks stack etc. are in SDRAM, it
 			   MUST be done directly after sdram_init() or not at all. The FreeRTOS code
 			   does not take kindly to messing around with the SDRAM contents after it
 			   has used it. */
-	  led_set_red(1);
-	  sdram_clear();
-	  led_set_red(0);
-  }
+		led_set_red(1);
+		sdram_clear();
+		led_set_red(0);
+	}
 
-  if (!power_init ())
-    {
-      led_halt_blinking (1);
-    }
-  power_on ();
-  
-
-  xTaskCreate (watchdog_restart_task, (signed portCHAR *) "WATCHDOG",
-	       TASK_WATCHDOG_STACK, NULL, TASK_WATCHDOG_PRIORITY, NULL);
-
-  xTaskCreate (vUSBCDCTask, (signed portCHAR *) "USB        ", TASK_USB_STACK,
-	       NULL, TASK_USB_PRIORITY, NULL);
-
-  /* vInitProtocolLayer (); */
-
-  //xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 0", 512, (void*)0, NEAR_IDLE_PRIORITY, NULL);
-  //xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 1", 512, (void*)3000, NEAR_IDLE_PRIORITY, NULL);
-  //xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 2", 512, (void*)4000, NEAR_IDLE_PRIORITY, NULL);
-  //xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 2", 512, (void*)5000, NEAR_IDLE_PRIORITY, NULL);
-  //xTaskCreate(eink_traffic_test_task, (signed portCHAR *) "EINK TRAFFIC", 512, NULL, NEAR_IDLE_PRIORITY, NULL);
-  
-  /*xTaskCreate (demo_task, (signed portCHAR *) "LED_DEMO", 512,
+	if (!power_init ()) {
+		led_halt_blinking (1);
+	}
+	power_on ();
+	
+	
+	xTaskCreate (watchdog_restart_task, (signed portCHAR *) "WATCHDOG",
+			TASK_WATCHDOG_STACK, NULL, TASK_WATCHDOG_PRIORITY, NULL);
+	
+	xTaskCreate (vUSBCDCTask, (signed portCHAR *) "USB        ", TASK_USB_STACK,
+			NULL, TASK_USB_PRIORITY, NULL);
+	
+	/* vInitProtocolLayer (); */
+	
+	//xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 0", 512, (void*)0, NEAR_IDLE_PRIORITY, NULL);
+	//xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 1", 512, (void*)3000, NEAR_IDLE_PRIORITY, NULL);
+	//xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 2", 512, (void*)4000, NEAR_IDLE_PRIORITY, NULL);
+	//xTaskCreate (sdram_test_task, (signed portCHAR *) "SDRAM_DEMO 2", 512, (void*)5000, NEAR_IDLE_PRIORITY, NULL);
+	//xTaskCreate(eink_traffic_test_task, (signed portCHAR *) "EINK TRAFFIC", 512, NULL, NEAR_IDLE_PRIORITY, NULL);
+	
+	/*xTaskCreate (demo_task, (signed portCHAR *) "LED_DEMO", 512,
      NULL, NEAR_IDLE_PRIORITY, NULL); */
-  /*xTaskCreate (sdcard_test_task, (signed portCHAR *) "SDCARD_DEMO", 1024,
-	       NULL, NEAR_IDLE_PRIORITY, NULL);*/
-  /*xTaskCreate (flash_demo_task, (signed portCHAR *) "FLASH DEMO", 512,
+	/*xTaskCreate (sdcard_test_task, (signed portCHAR *) "SDCARD_DEMO", 1024,
+			NULL, NEAR_IDLE_PRIORITY, NULL);*/
+	/*xTaskCreate (flash_demo_task, (signed portCHAR *) "FLASH DEMO", 512,
      NULL, NEAR_IDLE_PRIORITY, NULL); */
-  /*xTaskCreate (pn532_demo_task, (signed portCHAR *) "PN532 DEMO TASK", TASK_PN532_STACK,
+	/*xTaskCreate (pn532_demo_task, (signed portCHAR *) "PN532 DEMO TASK", TASK_PN532_STACK,
      NULL, TASK_PN532_PRIORITY, NULL);*/
-
-
-  eink_interface_init();
-  //ad7147_init();
-  //accelerometer_init();
-  //DFS_Init ();
-  //pn532_init();
-  //libnfc_demo_init();
-  //picc_emu_init();
-  //ebook_init();
-  paint_init();
-
-  //slider_register_slider_update_callback(slider_update_cb);
-  
-  led_set_green (1);
-
-  vTaskStartScheduler ();
-
-  while(1);
+	
+	
+	eink_interface_init();
+	ad7147_init();
+	//accelerometer_init();
+	//DFS_Init ();
+	//pn532_init();
+	//libnfc_demo_init();
+	//picc_emu_init();
+	//ebook_init();
+	paint_init();
+	
+	//slider_register_slider_update_callback(slider_update_cb);
+	
+	led_set_green (1);
+	
+	vTaskStartScheduler ();
+	
+	while(1);
 }
