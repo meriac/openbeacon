07db149f95fe458bc160f587c985f3669092199e
Milosch Meriac <meriac@openbeacon.de>
2010-05-24 10:41:28 +0000

    added blinking LED on RX

--- firmware/lpc13xx/uart/src/uarttest.c
+++ firmware/lpc13xx/uart/src/uarttest.c
@@ -9,6 +9,8 @@
  *
 ******************************************************************************/
 #include "LPC13xx.h"
+#include "config.h"
+#include "gpio.h"
 #include "uart.h"
 
 extern volatile uint32_t UARTCount;
@@ -16,6 +18,7 @@ extern volatile uint8_t UARTBuffer[BUFSIZE];
 
 int main(void)
 {
+    volatile int i;
     /* Basic chip initialization is taken care of in SystemInit() called
      * from the startup code. SystemInit() and chip settings are defined
      * in the CMSIS system_<part family>.c file.
@@ -24,12 +27,22 @@ int main(void)
     /* NVIC is installed inside UARTInit file. */
     UARTInit(115200);
 
+    /* Initialize GPIO (sets up clock) */
+    GPIOInit();
+
+    /* Set LED port pin to output */
+    GPIOSetDir(LED_PORT, LED_BIT, 1);
+
     while (1) {			/* Loop forever */
 	if (UARTCount != 0) {
 	    LPC_UART->IER = IER_THRE | IER_RLS;	/* Disable RBR */
 	    UARTSend((uint8_t *) UARTBuffer, UARTCount);
 	    UARTCount = 0;
 	    LPC_UART->IER = IER_THRE | IER_RLS | IER_RBR;	/* Re-enable RBR */
+
+	    GPIOSetValue(LED_PORT, LED_BIT, LED_ON);
+	    for(i=0;i<10000;i++);
+	    GPIOSetValue(LED_PORT, LED_BIT, LED_OFF);
 	}
     }
 }
