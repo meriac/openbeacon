14364dc343b0a0bb90d42283d8e510245649fd7b
Milosch Meriac <meriac@openbeacon.de>
2010-07-17 02:21:55 +0000

    rfid tree initial check in

---
+++ firmware/lpc13xx/rfid/inc/debug_printf.h
@@ -0,0 +1,18 @@
+/*
+ *  linux/lib/vsprintf.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
+
+/* vsprintf.c -- Lars Wirzenius & Linus Torvalds. */
+/*
+ * Wirzenius wrote this portably, Torvalds fucked it up :-)
+ */
+
+#ifndef __DEBUG_PRINTF_H__
+#define __DEBUG_PRINTF_H__
+
+extern void debug_printf (const char *fmt, ...);
+extern void hex_dump (const unsigned char *buf, unsigned int addr, unsigned int len);
+
+#endif/*__DEBUG_PRINTF_H__*/
