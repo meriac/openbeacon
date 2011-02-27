487829b727f780a9ad250f934a03db10bdedf89b
Milosch Meriac <meriac@bitmanufaktur.de>
2011-02-27 01:42:29 +0100

    added xxtea decode function

--- firmware/lpc13xx/core/openbeacon/inc/xxtea.h
+++ firmware/lpc13xx/core/openbeacon/inc/xxtea.h
@@ -31,7 +31,7 @@
 #ifndef __XXTEA_H__
 #define __XXTEA_H__
 
-extern void xxtea_decode (uint32_t * v, uint32_t length, const uint32_t *tea_key);
-extern void xxtea_shuffle (uint32_t * v, uint32_t length);
+extern void xxtea_decode (uint32_t * v, uint32_t length, const uint32_t * tea_key);
+extern void xxtea_encode (uint32_t * v, uint32_t length, const uint32_t * tea_key);
 
 #endif/*__XXTEA_H__*/
--- firmware/lpc13xx/core/openbeacon/src/xxtea.c
+++ firmware/lpc13xx/core/openbeacon/src/xxtea.c
@@ -31,19 +31,62 @@
 #include <openbeacon.h>
 #include "xxtea.h"
 
-#define MX  ( (((z>>5)^(y<<2))+((y>>3)^(z<<4)))^((sum^y)+(tea_key[(p&3)^e]^z)) )
-#define DELTA 0x9e3779b9UL
+static const uint32_t DELTA = 0x9e3779b9UL;
+
+#define MX ( (((z>>5)^(y<<2))+((y>>3)^(z<<4)))^((sum^y)+(tea_key[(p&3)^e]^z)) );
+
+static void
+xxtea_shuffle (uint32_t * v, uint32_t length)
+{
+  while (length--)
+    {
+      *v = htonl (*v);
+      v++;
+    }
+}
 
 void
-xxtea_decode (uint32_t * v, uint32_t length, const uint32_t * tea_key)
+xxtea_encode (uint32_t * v, uint32_t length, const uint32_t * tea_key)
 {
-  uint32_t z, y = v[0], sum = 0, e, p, q;
+  uint32_t z, y, sum, e, p, q;
 
   if (!v || !tea_key)
     return;
 
+  xxtea_shuffle (v, length);
+
+  q = 6 + 52 / length;
+  sum = 0;
+  z = v[length - 1];
+  do
+    {
+      sum += DELTA;
+      e = (sum >> 2) & 3;
+
+      for (p = 0; p < (length - 1); p++)
+	y = v[p + 1], z = v[p] += MX;
+
+      y = v[0];
+      z = v[length - 1] += MX;
+    }
+  while (--q);
+
+  xxtea_shuffle (v, length);
+}
+
+void
+xxtea_decode (uint32_t * v, uint32_t length, const uint32_t * tea_key)
+{
+  uint32_t z, y, sum, e, p, q;
+
+  if (!v || !tea_key)
+    return;
+
+  xxtea_shuffle (v, length);
+
   q = 6 + 52 / length;
   sum = q * DELTA;
+  y = v[0];
   while (sum)
     {
       e = (sum >> 2) & 3;
@@ -54,14 +97,6 @@ xxtea_decode (uint32_t * v, uint32_t length, const uint32_t * tea_key)
       y = v[0] -= MX;
       sum -= DELTA;
     }
-}
 
-void
-xxtea_shuffle (uint32_t * v, uint32_t length)
-{
-  while (length--)
-    {
-      *v = htonl (*v);
-      v++;
-    }
+    xxtea_shuffle (v, length);
 }
