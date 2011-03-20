34175c0d032b0df4118e500eddd773ba73a8cbb5
Milosch Meriac <meriac@bitmanufaktur.de>
2011-03-21 00:42:52 +0100

    removed dead GPIO IRQ code

--- firmware/lpc13xx/core/peripherals/inc/gpio.h
+++ firmware/lpc13xx/core/peripherals/inc/gpio.h
@@ -12,23 +12,8 @@
 #ifndef __GPIO_H
 #define __GPIO_H
 
-#define PORT0		0
-#define PORT1		1
-#define PORT2		2
-#define PORT3		3
-
-void GPIO_IRQHandler (void);
-void GPIOInit (void);
-void GPIOSetInterrupt (uint32_t portNum, uint32_t bitPosi, uint32_t sense,
-		       uint32_t single, uint32_t event);
-void GPIOIntEnable (uint32_t portNum, uint32_t bitPosi);
-void GPIOIntDisable (uint32_t portNum, uint32_t bitPosi);
-uint32_t GPIOIntStatus (uint32_t portNum, uint32_t bitPosi);
-void GPIOIntClear (uint32_t portNum, uint32_t bitPosi);
-
-static LPC_GPIO_TypeDef (*const LPC_GPIO[4]) =
-{
-LPC_GPIO0, LPC_GPIO1, LPC_GPIO2, LPC_GPIO3};
+extern void GPIOInit (void);
+extern LPC_GPIO_TypeDef (*const LPC_GPIO[4]);
 
 /*****************************************************************************
 ** Function name:		GPIOSetValue
