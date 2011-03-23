aff13884d041e509a932e782eaefd7986984c2d7
Milosch Meriac <meriac@bitmanufaktur.de>
2011-03-23 19:46:50 +0100

    moved firmware/at91sam7/openbeacon-openpicc2 to OpenPICC2 branch

--- firmware/at91sam7/openbeacon-openpicc2/application/ebook/ebook.h
+++
@@ -1,6 +0,0 @@
-#ifndef EBOOK_H_
-#define EBOOK_H_
-
-extern int ebook_init(void);
-
-#endif /*EBOOK_H_*/
--- firmware/at91sam7/openbeacon-openpicc2/application/touch/ad7147.h
+++
@@ -1,6 +0,0 @@
-#ifndef AD7147_H_
-#define AD7147_H_
-
-extern int ad7147_init(void);
-
-#endif /*AD7147_H_*/
