5d941121a47fc3006b21eeecda89e572d9264274
Milosch Meriac <meriac@bitmanufaktur.de>
2011-03-23 19:42:21 +0100

    moved firmware/at91sam7/openbeacon-openpicc2 to OpenPICC2 branch

--- firmware/at91sam7/openbeacon-openpicc2/application/dosfs/sdcard.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDCARD_H_
-#define SDCARD_H_
-
-extern int DFS_Init(void);
-extern int DFS_OpenCard(void);
-extern u_int32_t DFS_ReadSector(u_int8_t unit, u_int8_t * buffer,
-								u_int32_t sector, u_int32_t count);
-
-#endif							/*SDCARD_H_ */
--- firmware/at91sam7/openbeacon-openpicc2/application/paint/paint.h
+++
@@ -1,6 +0,0 @@
-#ifndef PAINT_H_
-#define PAINT_H_
-
-extern int paint_init(void);
-
-#endif /*PAINT_H_*/
