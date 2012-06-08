88afa90cae3399e78d064bc376a46fb371b7f897
Milosch Meriac <meriac@bitmanufaktur.de>
2012-06-08 19:39:41 +0200

    added new broken flow control code - debugging now

--- firmware/lpc13xx/openpcd2-libnfc/src/main.c
+++ firmware/lpc13xx/openpcd2-libnfc/src/main.c
@@ -36,9 +36,8 @@ typedef enum
 	STATE_HEADER = -3,
 	STATE_WAKEUP = -4,
 	STATE_FIFOFLUSH = -5,
-	STATE_POSTAMBLE = -6,
-	STATE_PAYLOAD = -7,
-	STATE_FULLPACKET = -8
+	STATE_PAYLOAD = -6,
+	STATE_FLOWCTRL = -7
 } PN532_State;
 
 typedef struct {
@@ -49,19 +48,26 @@ typedef struct {
 	uint8_t data_prev;
 	uint8_t wakeup;
 	uint8_t crc;
+	uint8_t tfi;
 	PN532_State state;
 	uint8_t data[PN532_MAX_PACKET_SIZE + 1];
 } PN532_Packet;
 
-static uint8_t buffer_get[PN532_MAX_PACKET_SIZE + 1];
-static PN532_Packet buffer_put;
+static PN532_Packet buffer_put, buffer_get;
 
 static void
-packet_reset (PN532_Packet* pkt, uint8_t reserved)
+packet_init (PN532_Packet* pkt, uint8_t reserved, uint8_t tfi)
 {
-	memset(pkt, 0, sizeof(pkt));
+	memset(pkt, 0, sizeof(*pkt));
 	pkt->reserved = reserved;
+	pkt->tfi = tfi;
 	pkt->data_prev = 0x01;
+}
+
+static void
+packet_reset (PN532_Packet* pkt)
+{
+	packet_init (pkt, pkt->reserved, pkt->tfi);
 }
 
 static int
@@ -71,26 +77,26 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 	uint8_t len, lcs;
 	const uint8_t prefix[] = { 0x00, 0x00, 0xFF };
 
-	debug ("RX: <IN> 0x%02X [pos=%02u,state=%i, expected=%02i]\n", data, pkt->pos, pkt->state, pkt->expected);
-
 	res = pkt->state;
 
 	switch (pkt->state)
 	{
-		case STATE_FULLPACKET:
-		{
-			packet_reset (pkt, pkt->reserved );
-			/* intentionally no 'break;' */
-		}
-
 		case STATE_WAKEUP:
 		{
+			debug ("\nWAKEUP\n");
 			res = STATE_IDLE;
 			/* intentionally no 'break;' */
 		}
 
 		case STATE_IDLE:
 		{
+			/* if needed, delete packet from previous run */
+			if(pkt->pos)
+			{
+				packet_reset (pkt);
+				break;
+			}
+
 			/* scan for 0x00+0xFF prefix */
 			if (data==0xFF && pkt->data_prev==0x00)
 			{
@@ -98,9 +104,9 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 				/* add size of reserved+prefix to packet pos */
 				pkt->pos = pkt->reserved+sizeof(prefix);
 				/* expect at least a short frame */
-				pkt->expected = pkt->pos + 3;
+				pkt->expected = pkt->pos + 2;
 				/* switch to prefix reception mode */
-				res = STATE_PREFIX;
+				res = STATE_FLOWCTRL;
 				break;
 			}
 
@@ -127,24 +133,20 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 			break;
 		}
 
-		case STATE_PREFIX:
+		case STATE_FLOWCTRL:
 		{
 			pkt->data[pkt->pos++] = data;
 			if(pkt->pos>=pkt->expected)
 			{
-				hex_dump (pkt->data, 0, pkt->pos);
-
-				lcs = pkt->data[pkt->pos-2];
-				len = pkt->data[pkt->pos-3];
-
-				debug ("IR: valid packet with LEN=0x%02X LCS=0x%02X\n",len,lcs);
+				lcs = pkt->data[pkt->pos-1];
+				len = pkt->data[pkt->pos-2];
 
 				/* detected extended frame */
 				if (len == 0xFF && lcs == 0xFF)
 				{
 					debug ("IR: extended frame\n");
 					/* expect three more bytes for extended frame */
-					pkt->expected+=3;
+					pkt->expected+=4;
 					res = STATE_PREFIX_EXT;
 					break;
 				}
@@ -152,23 +154,35 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 				/* detected ACK frame */
 				if (len == 0xFF && lcs == 0x00)
 				{
-					debug ("IR: NACK frame\n");
-					res = STATE_POSTAMBLE;
+					res = pkt->pos;
 					break;
 				}
 
 				/* detected NACK frame */
 				if (len == 0x00 && lcs == 0xFF)
 				{
-					debug ("IR: ACK frame\n");
-					res = STATE_POSTAMBLE;
+					res = pkt->pos;
 					break;
 				}
+
+				pkt->expected++;
+				res = STATE_PREFIX;
+			}
+
+			break;
+		}
+
+		case STATE_PREFIX:
+		{
+			pkt->data[pkt->pos++] = data;
+			if(pkt->pos>=pkt->expected)
+			{
+				lcs = pkt->data[pkt->pos-2];
+				len = pkt->data[pkt->pos-3];
 
 				if (len == 0x01 && lcs == 0xFF)
 				{
 					pkt->expected+=len;
-					debug ("IR: ERROR frame [error=0x%02X]\n",pkt->data[pkt->pos-1]);
 					pkt->crc=pkt->data[pkt->pos-1];
 					res = STATE_PAYLOAD;
 					break;
@@ -178,28 +192,25 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 				if ( ((uint8_t)(len+lcs)) == 0 )
 				{
 					pkt->expected+=len;
-					debug ("IR: short frame [%u payload, %u total]\n", len, pkt->expected);
 
 					/*detect oversized packets */
 					if(pkt->expected > PN532_MAX_PACKET_SIZE)
 					{
-						debug ("IR: oversized frame\n");
-						packet_reset (pkt, pkt->reserved );
+						packet_reset (pkt);
 						res = STATE_IDLE;
 					}
 					else
 					{
 						/* check for TFI */
-						if (pkt->data[pkt->pos-1] == 0xD4)
+						if (pkt->data[pkt->pos-1] == pkt->tfi)
 						{
 							/* maintain CRC including TFI */
-							pkt->crc=0xD4;
+							pkt->crc=pkt->tfi;
 							res = STATE_PAYLOAD;
 						}
 						else
 						{
-							debug ("IR: wrong TFI [0x%02X]\n", pkt->data[pkt->pos-1]);
-							packet_reset (pkt, pkt->reserved );
+							packet_reset (pkt);
 							res = STATE_IDLE;
 						}
 					}
@@ -214,7 +225,7 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 		{
 			/* TODO: add extended frame support */
 			debug ("IR: extended frame is not yet supported\n");
-			packet_reset (pkt, pkt->reserved );
+			packet_reset (pkt);
 			res = STATE_IDLE;
 			break;
 		}
@@ -229,30 +240,11 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 				if(pkt->crc)
 				{
 					debug ("IR: packet CRC error [0x%02X]\n", pkt->crc);
-					packet_reset (pkt, pkt->reserved );
+					packet_reset (pkt);
 					res = STATE_IDLE;
 				}
 				else
-					res = STATE_POSTAMBLE;
-			}
-			break;
-		}
-
-		case STATE_POSTAMBLE:
-		{
-			if(data == 0x00)
-			{
-				debug ("IR: full packet - CRC OK\n");
-
-				/* return packet size */
-				pkt->data[pkt->pos++] = data;
-				res = pkt->pos;
-			}
-			else
-			{
-				debug ("IR: packet postamble error [0x%02X]\n", data);
-				packet_reset (pkt, pkt->reserved );
-				res = STATE_IDLE;
+					res = pkt->pos;
 			}
 			break;
 		}
@@ -260,17 +252,23 @@ packet_put (PN532_Packet* pkt, uint8_t data)
 		default:
 		{
 			debug ("IR: unknown state!!!\n");
-			packet_reset (pkt, pkt->reserved );
+			packet_reset (pkt);
 			res = STATE_IDLE;
 		}
 	}
 
 	pkt->data_prev = data;
-	pkt->state = (res>0) ? STATE_FULLPACKET:res;
-
-	debug ("RX: <OT> 0x%02X [pos=%02u,state=%i, expected=%02i]\n", data, pkt->pos, pkt->state, pkt->expected);
-
+	pkt->state = (res>0) ? STATE_IDLE:res;
 	return res;
+}
+
+void
+dump_packet (uint8_t* data, int count)
+{
+	int i;
+	for (i = 0; i < count; i++)
+		debug_printf ("%c%02X", i == 6 ? '*' : ' ', *data++);
+	debug_printf ("\n");
 }
 
 int
@@ -309,41 +307,39 @@ main (void)
 	while (1)
 	{
 		if (((i = rfid_write (&data, sizeof (data))) == 0) &&
-			((i = rfid_read (buffer_get, PN532_FIFO_SIZE))) > 0)
+			((i = rfid_read (buffer_get.data, PN532_FIFO_SIZE))) > 0)
 			break;
 
-		debug ("res=%i\n", i);
+		debug_printf ("fw_res=%i\n", i);
 		pmu_wait_ms (490);
 		GPIOSetValue (LED_PORT, LED_BIT, LED_ON);
 		pmu_wait_ms (10);
 		GPIOSetValue (LED_PORT, LED_BIT, LED_OFF);
 	}
 
-	if (buffer_get[1] == 0x32)
+	if (buffer_get.data[1] == 0x32)
 		debug_printf ("PN532 firmware version: v%i.%i\n",
-					  buffer_get[2], buffer_get[3]);
+					  buffer_get.data[2], buffer_get.data[3]);
 	else
 		debug ("Unknown firmware version\n");
 
-	/* run RFID loop */
-	buffer_get[0] = 0x01;
-	t = 0;
+	/* reset FIFO buffers */
+	packet_init ( &buffer_get, 0, 0xD5 );
+	packet_init ( &buffer_put, 1, 0xD4 );
 
-	packet_reset ( &buffer_put, 1 );
+	/* run RFID loop */
+	t = 0;
 	while (1)
 	{
 		if (!GPIOGetValue (PN532_IRQ_PORT, PN532_IRQ_PIN))
 		{
 			GPIOSetValue (LED_PORT, LED_BIT, (t++) & 1);
 
-			debug ("RX: ");
-
 			data = 0x03;
 
 			spi_txrx (SPI_CS_PN532 | SPI_CS_MODE_SKIP_CS_DEASSERT, &data,
 					  sizeof (data), NULL, 0);
 
-			i = 0;
 			while (!GPIOGetValue (PN532_IRQ_PORT, PN532_IRQ_PIN))
 			{
 				spi_txrx ((SPI_CS_PN532 ^ SPI_CS_MODE_SKIP_TX) |
@@ -351,38 +347,34 @@ main (void)
 						  SPI_CS_MODE_SKIP_CS_DEASSERT, NULL, 0,
 						  &data, sizeof (data));
 
-				usb_putchar (data);
-				debug (" %02X", data);
-				i++;
+				if ( (res = packet_put(&buffer_get, data))>0 )
+				{
+					buffer_get.data[res++] = 0x00;
+					usb_get ( buffer_get.data, res );
+					usb_flush ();
+#ifdef  DEBUG
+					debug ("RX: ");
+					dump_packet (buffer_get.data, res);
+#endif/*DEBUG*/
+				}
 			}
+
 			spi_txrx (SPI_CS_PN532 | SPI_CS_MODE_SKIP_CS_ASSERT, NULL, 0,
 					  NULL, 0);
-
-			if (i)
-			{
-				usb_putchar (0x00);
-				usb_flush ();
-				debug ("-00");
-			}
-			debug ("\n");
 		}
 
-		if ((res = usb_getchar ()) >= 0)
+		while ((res = usb_getchar ()) >= 0)
 		{
 			if((count = packet_put(&buffer_put, (uint8_t)res))>0)
 			{
 				GPIOSetValue (LED_PORT, LED_BIT, (t++) & 1);
-
-#ifdef  DEBUG
-				uint8_t *p = &buffer_put.data[1];
-				debug ("IT: ");
-				for (i = 0; i < (count-1); i++)
-					debug ("%c%02X", i == 6 ? '*' : ' ', *p++);
-				debug ("\n");
-#endif/*DEBUG*/
-
 				buffer_put.data[0] = 0x01;
+				buffer_put.data[count++] = 0x00;
 				spi_txrx (SPI_CS_PN532, buffer_put.data, count, NULL, 0);
+#ifdef  DEBUG
+				debug ("TX: ");
+				dump_packet (&buffer_put.data[1], count-1);
+#endif/*DEBUG*/
 			}
 			else
 				switch(count)
