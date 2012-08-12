d63d08265ecf74aea345cb367912bb47b7881a07
Milosch Meriac <meriac@bitmanufaktur.de>
2012-08-12 16:27:55 +0200

    removed debug_printf reference again

--- firmware/lpc13xx/core/openbeacon/src/printf.h
+++ firmware/lpc13xx/core/openbeacon/src/printf.h
@@ -107,15 +107,7 @@ regs Kusti, 23.10.2004
 
 #include <stdarg.h>
 
-void init_printf (void *putp, void (*putf) (void *, char));
+extern void tfp_sprintf (char *s, char *fmt, ...);
+extern void tfp_format (void *putp, void (*putf) (void *, char), char *fmt, va_list va);
 
-void tfp_printf (char *fmt, ...);
-void tfp_sprintf (char *s, char *fmt, ...);
-
-void tfp_format (void *putp, void (*putf) (void *, char), char *fmt,
-				 va_list va);
-
-#define printf tfp_printf
-#define sprintf tfp_sprintf
-
-#endif
+#endif/*__TFP_PRINTF__*/
