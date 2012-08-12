82ec77dbf5b15688d57dcf4c7b276aaa1c78bc51
Milosch Meriac <meriac@bitmanufaktur.de>
2012-08-12 18:22:45 +0200

    added copyright headers

--- firmware/lpc13xx/core/openbeacon/src/debug_printf.c
+++ firmware/lpc13xx/core/openbeacon/src/debug_printf.c
@@ -1,3 +1,26 @@
+/***************************************************************
+ *
+ * OpenBeacon.org - debug_printf wrapper for ts_printf
+ *
+ * Copyright 2010-2012 Milosch Meriac <meriac@openbeacon.de>
+ *
+ ***************************************************************
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
 #include <openbeacon.h>
 #if !defined UART_DISABLE || defined ENABLE_USB_FULLFEATURED
 #include <ctype.h>
