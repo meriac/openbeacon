a007e0851aedea60e8ad0f2b2d31e6c2fe751299
Milosch Meriac <meriac@bitmanufaktur.de>
2012-10-06 07:56:37 +0200

    fixed JSON to BSON converter

--- host/services/openbeacon-tracker/src/filter-mongodb.cpp
+++ host/services/openbeacon-tracker/src/filter-mongodb.cpp
@@ -37,6 +37,7 @@
 
 #define LOG "FILTER_MONGODB "
 
+#define MAX_NUM_SIZE 16
 #define MEMORY_INCREMENT 4096
 
 mongo g_mongo;
@@ -44,87 +45,74 @@ const int g_mongo_port = 27017;
 const char g_mongo_host[] = "127.0.0.1";
 
 void
-process_json_object (const char *json_key, struct json_object *json_obj,
-					 int level)
+process_json2bson (struct json_object *json_obj, const char *json_key,
+				   bson * b)
 {
-	char *key;
+	int n, i;
+	char *key, index[MAX_NUM_SIZE];
 	struct lh_entry *entry;
 	struct json_object *val;
 	enum json_type type = json_object_get_type (json_obj);
 
-	level++;
-
-	printf ("\tkey=%s val=%s type=%i level=%i\n", json_key,
-			json_object_get_string (json_obj), type, level);
-
 	switch (type)
 	{
 		case json_type_boolean:
+			bson_append_bool (b, json_key,
+							  json_object_get_boolean (json_obj));
 			break;
+
 		case json_type_double:
+			bson_append_double (b, json_key,
+								json_object_get_double (json_obj));
 			break;
+
 		case json_type_int:
+			bson_append_int (b, json_key, json_object_get_int (json_obj));
 			break;
+
 		case json_type_object:
 			{
-				printf ("###OBJECT:\n");
+				if (json_key)
+					bson_append_start_object (b, json_key);
+
 				for (entry = json_object_get_object (json_obj)->head;
 					 (entry ? (key = (char *) entry->k, val =
-						 (struct json_object *) entry->v, entry) : 0);
+							   (struct json_object *) entry->v, entry) : 0);
 					 entry = entry->next)
 				{
-					process_json_object (key, val, level);
+					process_json2bson (val, key, b);
 				}
-				printf ("OBJECT###\n");
+
+				if (json_key)
+					bson_append_finish_object (b);
 				break;
 			}
+
 		case json_type_array:
-//          json_object_get_array(json_obj);
-			break;
-		case json_type_string:
-			break;
-		default:
-			break;
-	}
-
-}
-
-void
-process_json (struct json_object *json_obj)
-{
-	char *key;
-	struct lh_entry *entry;
-	struct json_object *val;
-	enum json_type type = json_object_get_type (json_obj);
-
-	switch (type)
-	{
-		case json_type_boolean:
-			break;
-		case json_type_double:
-			break;
-		case json_type_int:
-			break;
-		case json_type_object:
 			{
-				printf ("###OBJECT:\n");
-				for (entry = json_object_get_object (json_obj)->head;
-					 (entry
-					  ? (key = (char *) entry->k, val =
-						 (struct json_object *) entry->v, entry) : 0);
-					 entry = entry->next)
+				bson_append_start_array (b, json_key);
+
+				n = json_object_array_length (json_obj);
+				for (i = 0; i < n; i++)
 				{
-					process_json_object (key, val, 0);
+					snprintf(index, sizeof(index), "%i", i);
+
+					if ((val = json_object_array_get_idx (json_obj, i)) == NULL)
+						bson_append_null (b, index);
+					else
+						process_json2bson (val, index, b);
 				}
-				printf ("OBJECT###\n");
+
+				bson_append_finish_array (b);
 				break;
 			}
-		case json_type_array:
-//          json_object_get_array(json_obj);
-			break;
+
 		case json_type_string:
+			bson_append_string (b, json_key, json_object_get_string (json_obj));
 			break;
+
 		default:
+			bson_append_null (b, json_key);
 			break;
 	}
 }
@@ -137,7 +125,7 @@ main (int argc, char *argv[])
 	char *buffer, *p;
 	struct json_tokener *tok;
 	struct json_object *json;
-
+	bson b;
 	if (argc != 2)
 	{
 		fprintf (stderr, LOG " usage: %s mongodb_table_name\n", argv[0]);
@@ -155,7 +143,9 @@ main (int argc, char *argv[])
 	if ((res =
 		 mongo_connect (&g_mongo, g_mongo_host, g_mongo_port)) != MONGO_OK)
 	{
-		fprintf (stderr, LOG " can't open mongo_db (host %s:%i)\n",
+		fprintf (stderr,
+				 LOG
+				 " can't open mongo_db (host %s:%i)\n",
 				 g_mongo_host, g_mongo_port);
 		json_tokener_free (tok);
 		return -3;
@@ -169,23 +159,27 @@ main (int argc, char *argv[])
 	{
 		/* convert to 8 bits */
 		c[0] = (uint8_t) data;
-
 		/* echo everything to maintain pipe chain */
 //      putchar (c[0]);
-
 		/* find start of new object */
 		if (pos && c[2] == '\n' && c[1] == '}' && c[0] == ',')
 		{
 			/* terminate string */
 			*p = 0;
-
 			json = json_tokener_parse_ex (tok, buffer, pos);
 			if (tok->err != json_tokener_success)
 				fprintf (stderr, LOG "JSON conversion error\n");
 			else
-				process_json (json);
+			{
+				bson_init (&b);
+				process_json2bson (json, NULL, &b);
+				bson_finish (&b);
+				printf ("\n###\n");
+				bson_print (&b);
+//              mongo_insert (&g_mongo, argv[0], &b);
+				bson_destroy (&b);
+			}
 			json_tokener_reset (tok);
-
 			/* reset object buffer */
 			p = buffer;
 			pos = 0;
@@ -217,6 +211,5 @@ main (int argc, char *argv[])
 
 	mongo_destroy (&g_mongo);
 	json_tokener_free (tok);
-
 	return res;
 }
