d8e1b5f2d7de3d2258f491dc5ecaf5205cd0a29b
Milosch Meriac <meriac@bitmanufaktur.de>
2012-10-19 02:28:32 +0200

    removed old crap

--- firmware/lpc13xx/openpcd2-audio/src/main.c
+++ firmware/lpc13xx/openpcd2-audio/src/main.c
@@ -28,7 +28,6 @@
 #define MIFARE_KEY_SIZE 6
 const unsigned char mifare_key[MIFARE_KEY_SIZE] =
 	{ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-unsigned char test_signal = 0;
 
 static void
 rfid_hexdump (const void *buffer, int size)
@@ -45,43 +44,14 @@ rfid_hexdump (const void *buffer, int size)
 	debug_printf (" [size=%02i]\n", size);
 }
 
-void
-CDC_GetCommand (unsigned char *command)
-{
-	if (!command[1])
-		switch (command[0])
-		{
-			case '+':
-				test_signal++;
-				break;
-			case '-':
-				test_signal--;
-				break;
-			case 'B':
-				/* increment U.FL test bus number */
-				test_signal = (test_signal & ~0x7) + (1 << 3);
-				break;
-			case 'b':
-				/* decrement U.FL test bus number */
-				test_signal = (test_signal & ~0x7) - (1 << 3);
-				break;
-			case '0':
-				test_signal = 0;
-				break;
-		}
-}
-
 static void
 loop_rfid (void)
 {
-	int res, old_test_signal = -1, present;
-	static unsigned char data[80], bus, signal;
-	static unsigned char oid[4];
+	int res, present, i;
+	static unsigned char data[80];
 
 	/* fully initialized */
 	GPIOSetValue (LED1_PORT, LED1_BIT, LED_ON);
-
-	while(1);
 
 	/* read firmware revision */
 	debug_printf ("\nreading firmware version...\n");
@@ -101,11 +71,9 @@ loop_rfid (void)
 	else
 		debug_printf ("v%i.%i\n", data[2], data[3]);
 
-	/* show card response on U.FL */
-	test_signal = (25 << 3) | 2;
 	/* enable debug output */
 	GPIOSetValue (LED1_PORT, LED1_BIT, LED_ON);
-	present = 0;
+	i = present = 0;
 	while (1)
 	{
 		/* wait 1s */
@@ -113,9 +81,9 @@ loop_rfid (void)
 
 		if(present)
 			present--;
-/*		else
+		else
 			if((i++%10) == 0)
-				lcd_update_char (CHARACTER_START+((i/10)%26));*/
+				lcd_update_screen ('A'+((i/10)%26));
 
 		/* detect cards in field */
 		data[0] = PN532_CMD_InListPassiveTarget;
@@ -124,55 +92,8 @@ loop_rfid (void)
 		if (((res = rfid_execute (&data, 3, sizeof (data))) >= 11)
 			&& (data[1] == 0x01) && (data[2] == 0x01))
 		{
-			/* only for Mifare Classic cards */
-			if (data[3] == 0 && data[4] == 4 && data[6] >= 4)
-			{
-				memcpy (oid, &data[7], sizeof (oid));
-
-				data[0] = PN532_CMD_InDataExchange;
-				data[1] = 0x01;													/* card 1 */
-				data[2] = 0x60;													/* MIFARE authenticate A */
-				data[3] = 0x01;													/* block 1 */
-				/* MIFARE NFCID1 */
-				memcpy (&data[10], oid, sizeof (oid));
-				/* MIFARE default key 6*0xFF */
-				memcpy (&data[4], mifare_key, MIFARE_KEY_SIZE);
-
-				/* MIFARE Authenticate */
-				res = rfid_execute (&data, 14, sizeof (data));
-
-				if (res > 0)
-				{
-					rfid_hexdump (&data, res);
-
-					data[0] = PN532_CMD_InDataExchange;
-					data[1] = 0x01;												/* card 1 */
-					data[2] = 0x30;												/* MIFARE read 16 bytes */
-					data[3] = 0x01;												/* block 1 */
-
-					/* MIFARE Read */
-					res = rfid_execute (&data, 14, sizeof (data));
-
-					debug_printf ("\nMIFARE_READ:");
-					if (res == 18)
-						rfid_hexdump (&data[2], 16);
-					else
-						debug_printf (" failed [%i]\n", res);
-				}
-				else
-					debug_printf ("AUTH failed [%i]\n", res);
-
-				debug_printf ("MIFARE_CARD_ID:");
-				rfid_hexdump (oid, sizeof (oid));
-			}
-			else
-			{
-				debug_printf ("\nCARD_ID:");
-				rfid_hexdump (&data[7], data[6]);
-			}
-
-			lcd_update_char ('X');
-			present=5;
+			debug_printf ("\nCARD_ID:");
+			rfid_hexdump (&data[7], data[6]);
 
 			/* blink LED to indicate card */
 			GPIOSetValue (LED1_PORT, LED1_BIT, LED_ON);
@@ -191,24 +112,6 @@ loop_rfid (void)
 		data[1] = 0x01;															/* CfgItem = 0x01           */
 		data[2] = 0x00;															/* RF Field = off           */
 		rfid_execute (&data, 3, sizeof (data));
-
-		if (test_signal != old_test_signal)
-		{
-			old_test_signal = test_signal;
-
-			/* enable test singal output on U.FL sockets */
-			signal = test_signal & 0x7;
-			bus = (test_signal >> 3) & 0x1F;
-
-			rfid_write_register (0x6328, 0xFC);
-			/* select test bus signal */
-			rfid_write_register (0x6321, signal);
-			/* select test bus type */
-			rfid_write_register (0x6322, bus);
-			debug_printf ("UPDATED_DEBUG_OUTPUT\n");
-		}
-		/* display current test signal ID */
-		debug_printf ("TEST_SIGNAL_ID: %02i.%i\n", bus, signal);
 	}
 }
 
@@ -275,8 +178,7 @@ main (void)
 	/* Init Audio */
 	audio_init ();
 
-	while(1)
-		spell("FAR OUT IN THE UNCHARTED BACKWATERS OF THE UNFASHIONABLE END OF THE WESTERN SPIRAL ARM OF THE GALAXY LIES A SMALL UNREGARDED YELLOW SUN   ORBITING THIS AT A DISTANCE OF ROUGHLY NINETY-TWO MILLION MILES IS AN UTTERLY INSIGNIFICANT LITTLE BLUE GREEN PLANET WHOSE APE-DESCENDED LIFE FORMS ARE SO AMAZINGLY PRIMITIVE THAT THEY STILL THINK DIGITAL WATCHES ARE A PRETTY NEAT IDEA  ");
+	spell("HELLO WORLD");
 
 	/* RUN RFID loop */
 	loop_rfid ();
