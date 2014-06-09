7df1dbd1074cf89c1cc6eb56cf58baa8148e4f22
Milosch Meriac <milosch@meriac.com>
2014-06-09 23:28:21 +0100

    added RGB LED support

--- firmware/lpc13xx/rgb-sensor/src/main.c
+++ firmware/lpc13xx/rgb-sensor/src/main.c
@@ -23,24 +23,52 @@
 #include <openbeacon.h>
 #include "usbserial.h"
 
-int
-main (void)
-{
-	volatile int t;
+#define LED_OFF 0x00
+#define LED_R   0x01
+#define LED_G   0x02
+#define LED_B   0x04
 
-	/* wait on boot - debounce */
-	for (t = 0; t < 2000000; t++);
+static inline void set_rgb(uint8_t rgb)
+{
+	LPC_GPIO1->MASKED_ACCESS[0xE] = rgb << 1;
+}
+
+static void init_hardware(void)
+{
+	/* init wait timer */
+	pmu_init();
 
 	/* Initialize GPIO (sets up clock) */
 	GPIOInit ();
 	GPIOSetDir (BUT1_PORT, BUT1_PIN, 0);
 	GPIOSetDir (BUT2_PORT, BUT2_PIN, 0);
 
+	/* initialize LED RGB port */
+	LPC_GPIO1->DIR = 0x0E;
+	LPC_IOCON->JTAG_TDO_PIO1_1   = 0x81;
+	LPC_IOCON->JTAG_nTRST_PIO1_2 = 0x81;
+	LPC_IOCON->ARM_SWDIO_PIO1_3  = 0x81;
+
 	/* CDC USB Initialization */
 	init_usbserial ();
+}
+
+int
+main (void)
+{
+	init_hardware();
 
 	while(1)
 	{
+		set_rgb(LED_R);
+		pmu_wait_ms(500);
+		set_rgb(LED_G);
+		pmu_wait_ms(500);
+		set_rgb(LED_B);
+		pmu_wait_ms(500);
+		set_rgb(LED_OFF);
+		pmu_wait_ms(1000);
+
 		debug_printf("Hello World\n\r");
 	}
 
